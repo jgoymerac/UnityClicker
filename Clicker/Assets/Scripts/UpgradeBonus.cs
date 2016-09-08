01bef1d8a336b71158f077819d3ccb324527c6c8
jgoymerac <jgoymerac@gmail.com>
2016-09-08 14:26:37 -0500

    UpGradeBonus Added
    Added a single upgradeBonus button

---
+++ Clicker/Assets/Scripts/UpgradeBonus.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpgradeBonus : MonoBehaviour {
+
+    public Click click;
+    public float cost;
+    public float power;
+    public string itemName;
+    public UpgradeManager _count;
+
+    void Start() {
+        _count = GetComponentInParent<UpgradeManager>();
+    }
+    void Update() {
+
+    }
+    public void PurchasedUpgrade() {
+        if (click.energy >= cost) {
+            click.energy -= cost;
+            click.energyPerClick += _count.count * _count.clickPower;
+            _count.clickPower *= 2;
+        }
+    }
+
+
+
+}
